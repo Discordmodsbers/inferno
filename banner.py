5c8b1ae3d9816cd78d79f60e7bfabef1a9f06a77
Discordmodsbers <94165954+Discordmodsbers@users.noreply.github.com>
2023-01-12 08:35:46 -0500

    Added this so i can edit my menus better.

---
+++ banner.py
@@ -0,0 +1,10 @@
+from Inferno import *
+cls()
+updatetext = f"""██    ██ ██████  ██████   █████  ████████ ███████     ██ 
+██    ██ ██   ██ ██   ██ ██   ██    ██    ██          ██ 
+██    ██ ██████  ██   ██ ███████    ██    █████       ██ 
+██    ██ ██      ██   ██ ██   ██    ██    ██             
+ ██████  ██      ██████  ██   ██    ██    ███████     ██ 
+Please use ['{green}Y{reset}'] To Update
+Please use ['{red}ENTER{reset}'] To Ignore
+"""
